d95bf7ab63d0eca4aa5c4d40f26c546289436a66
DedStarui <136914518+DedStarui@users.noreply.github.com>
2025-05-03 17:32:01 +0300

    Update game.py

--- game.py
+++ game.py
@@ -5,13 +5,14 @@ display.set_caption("shooter")
 background = transform.scale(image.load("space.jpg"), (700, 500))
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -23,14 +24,18 @@ class Player(GameSprite):
         if keys[K_d] and self.rect.x < 633:
             self.rect.x += self.speed
     def fire(self):
-        pass
+        bullet = Bullet("bullet2.png", self.rect.centerx, self.rect.top, 110, 110, 8)
+        bullets.add(bullet)
 
-player = Player("player.png", 300, 400, 8)
+
+
+player = Player("player.png", 300, 400, 65, 65, 12)
 monsters = sprite.Group()
 font.init()
 font1 = font.Font(None, 36)
 lost = 0
 text_lose = font1.render("Пропущено" + str(lost), 1, (255, 255, 255))
+
 
 class Enemy(GameSprite):
     def update(self):
@@ -41,10 +46,20 @@ class Enemy(GameSprite):
             self.rect.y = 0
             lost += 1
 
-
-for i in range(1, 15):
-    monster = Enemy("enemy.png", randint(70, 670), 500, randint(3, 6))
+class Bullet(GameSprite):
+    def update(self):
+        
+        self.rect.y -= self.speed
+        if self.rect.y < 0:
+            self.kill()
+        
+for i in range(1, 10):
+    monster = Enemy("enemy.png", randint(70, 670), 500, 65, 65, randint(3, 5))
     monsters.add(monster)
+
+bullets = sprite.Group()
+
+
 
 game = True
 game_finish = False
@@ -53,6 +68,10 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                player.fire()
+
     if not game_finish:
         window.blit(background, (0 , 0))
         window.blit(text_lose, (50, 50) )
@@ -61,6 +80,7 @@ while game:
         player.reset()
         player.update()
         display.update()
+
         
     time.delay(30)
         
